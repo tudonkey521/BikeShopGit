5dab34965a8306893902c6c24f4068638bfc4314
TING013 <raymond.ting@bertelsmann.de>
2022-09-05 18:08:44 +0800

    first push

---
+++ src/test/java/com/example/BikeShop/BikeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BikeShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
